91b0740de900bf26895643e06da897b2962d883f
Alex Busch <69646582+abusch8@users.noreply.github.com>
2026-07-30 21:11:36 -0500

    Windows support for vimrc source

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,7 +7,15 @@ DEFAULT_ENABLE_ICONS = false
 _G.THEME = env.get("NVIM_THEME") or DEFAULT_THEME
 _G.ENABLE_ICONS = env.get("NVIM_ENABLE_ICONS") == "1" or DEFAULT_ENABLE_ICONS
 
-vim.cmd("source ~/.vimrc")
+local vimrc = vim.fn.expand("~/.vimrc")
+
+if vim.fn.has("win32") == 1 then
+    vimrc = vim.fn.expand("~/_vimrc")
+end
+
+if vim.fn.filereadable(vimrc) == 1 then
+    vim.cmd("source " .. vimrc)
+end
 
 require("config.lazy")
 require("config.options")
